5693d27017bc244434560fd59a11e77441dc3f8d
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-08-23 23:42:30 +0530

    Update mine.groovy

--- vars/mine.groovy
+++ vars/mine.groovy
@@ -2,6 +2,7 @@ def call() {
   node {
     def p = pipelineCfg()
          sh "echo ${p.branch}"
+         sh "echo ${p.test}"
      
     stage('Checkout') {
       sh "echo ${p.appname}"
@@ -9,6 +10,7 @@ def call() {
       sh "echo ${p.env}"
       sh "echo $BUILD_NUMBER"
       sh "echo ${p.branch}"
+      sh "echo ${p.test}"
     }
     
     
