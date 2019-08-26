18496228c8567af34ecd53246da492293227c4a8
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-08-27 02:54:39 +0530

    Update mine.groovy

--- vars/mine.groovy
+++ vars/mine.groovy
@@ -2,7 +2,7 @@ def call() {
   node {
     def p = pipelineCfg()
          sh """
-         cd cd /var/lib/jenkins/workspace/${JOB_NAME}/
+         cd /var/lib/jenkins/workspace/${JOB_NAME}/
          ls
          """
          sh "echo ${p.branch}"
