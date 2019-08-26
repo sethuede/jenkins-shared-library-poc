775e1cf9c7262ffa131c8b59c4fc82b6e3c78221
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-08-27 02:28:21 +0530

    Update pipelineCfg.groovy

--- vars/pipelineCfg.groovy
+++ vars/pipelineCfg.groovy
@@ -1,6 +1,6 @@
 def call() {
   sh "pwd"
   sh "ls"
-  Map pipelineCfg = readYaml(file: "${workspace}/pipeline.yaml")
+  Map pipelineCfg = readYaml(file: "${workspace}/${JOB_NAME}/pipeline.yaml")
   return pipelineCfg
 }
