134de12c650bb2ab3f38e337f4d08d34176eba10
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-08-27 02:31:38 +0530

    Update pipelineCfg.groovy

--- vars/pipelineCfg.groovy
+++ vars/pipelineCfg.groovy
@@ -1,6 +1,6 @@
 def call() {
   sh "pwd"
   sh "ls"
-  Map pipelineCfg = readYaml(file: "${workspace}/${JOB_NAME}/pipeline.yaml")
+  Map pipelineCfg = readYaml(file: "/var/lib/jenkins/workspace/${JOB_NAME}/pipeline.yaml")
   return pipelineCfg
 }
