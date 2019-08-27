7961cdaaefefda2f8f04bbe9cf523820d37a0d6c
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-08-28 02:38:44 +0530

    Update mine.groovy

--- vars/mine.groovy
+++ vars/mine.groovy
@@ -1,14 +1,16 @@
 def call() {
   node {
+    def branch = p.branch
+    def jobname = p.jobName
     def p = pipelineCfg()
        checkout([$class: 'GitSCM',
-                 branches: [[name: '*/${p.branch}']],
+                 branches: [[name: '*/${branch}']],
         doGenerateSubmoduleConfigurations: false,
         extensions: [],
         submoduleCfg: [],
         userRemoteConfigs: [[
             
-url: 'https://github.com/durgaprasad444/${p.jobName}.git'
+url: 'https://github.com/durgaprasad444/${jobname}.git'
              ]]])
          sh """
          cd /var/lib/jenkins/workspace/${JOB_NAME}/
