740866fabe69892e55d3aa18342439a42e0d0825
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-09-06 23:37:36 +0530

    Update pipeline_initialize.groovy

--- vars/pipeline_initialize.groovy
+++ vars/pipeline_initialize.groovy
@@ -1,11 +1,21 @@
 def call() {
   node {
-     stage('Checkout') {
-      
-    }
-  
-
-    def p = pipelineCfg()
+    def test = JOB_NAME
+    def (name, value) = test.split("/")
+    def (areho1, areho2) = value.split("_")
+    def maha = areho1.replaceFirst('-','/')
+       checkout([$class: 'GitSCM',
+                 branches: [[name: "*/${maha}"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [],
+        submoduleCfg: [],
+        userRemoteConfigs: [[
+            
+          url: "https://github.com/durgaprasad444/${name}.git"
+             ]]])
+   
+   
+   def p = pipelineCfg()
 
    switch(p.pipelineType) {
       case 'python':
