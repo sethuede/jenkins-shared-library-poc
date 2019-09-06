32d4226461e2479ad28058c7a2dd117bbf911fc0
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-09-06 23:39:31 +0530

    Update mine.groovy

--- vars/mine.groovy
+++ vars/mine.groovy
@@ -7,8 +7,6 @@ def call() {
          sh """
          cd /var/lib/jenkins/workspace/${JOB_NAME}/
          ls
-         echo ${name}
-         echo ${maha}
          """
          sh "echo ${p.branch}"
          sh "echo ${p.test}"
