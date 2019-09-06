42755acc254ed2f4e2b9efc09db0780eda1ae51e
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-09-06 23:24:31 +0530

    Update mine.groovy

--- vars/mine.groovy
+++ vars/mine.groovy
@@ -6,15 +6,7 @@ def call() {
     def (areho1, areho2) = value.split("_")
     def maha = areho1.replaceFirst('-','/')
      
-       checkout([$class: 'GitSCM',
-                 branches: [[name: "*/${maha}"]],
-        doGenerateSubmoduleConfigurations: false,
-        extensions: [],
-        submoduleCfg: [],
-        userRemoteConfigs: [[
-            
-          url: "https://github.com/durgaprasad444/${name}.git"
-             ]]])
+      checkout scm: [$class: 'GitSCM', branches: [[name: "*/${maha}"]], userRemoteConfigs: [[url: "https://github.com/durgaprasad444/${name}.git"]]]
     def p = pipelineCfg()
          sh """
          cd /var/lib/jenkins/workspace/${JOB_NAME}/
