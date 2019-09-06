8a0ab7f964f184f31878c950aeaf8887747d746a
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-09-06 23:11:20 +0530

    Update mine.groovy

--- vars/mine.groovy
+++ vars/mine.groovy
@@ -1,16 +1,13 @@
 def call() {
   node {
     
-    def p = pipelineCfg()
-    def branch = p.branch
-    def jobname = p.jobName
     def test = JOB_NAME
     def (name, value) = test.split("/")
     def (areho1, areho2) = value.split("_")
     def maha = areho1.replaceFirst('-','/')
      
        checkout([$class: 'GitSCM',
-                 branches: [[name: "*/${p.branch}"]],
+                 branches: [[name: "*/${maha}"]],
         doGenerateSubmoduleConfigurations: false,
         extensions: [],
         submoduleCfg: [],
@@ -18,6 +15,7 @@ def call() {
             
           url: "https://github.com/durgaprasad444/${name}.git"
              ]]])
+    def p = pipelineCfg()
          sh """
          cd /var/lib/jenkins/workspace/${JOB_NAME}/
          ls
