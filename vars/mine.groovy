5a9eee6c4d1695e56d25a2cf081563520f08652c
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-09-06 23:01:54 +0530

    Update mine.groovy

--- vars/mine.groovy
+++ vars/mine.groovy
@@ -6,6 +6,8 @@ def call() {
     def jobname = p.jobName
     def test = JOB_NAME
     def (name, value) = test.split("/")
+    def (areho1, areho2) = value.split("_")
+     
        checkout([$class: 'GitSCM',
                  branches: [[name: "*/${p.branch}"]],
         doGenerateSubmoduleConfigurations: false,
@@ -19,6 +21,7 @@ def call() {
          cd /var/lib/jenkins/workspace/${JOB_NAME}/
          ls
          echo ${name}
+         echo ${areho1}
          """
          sh "echo ${p.branch}"
          sh "echo ${p.test}"
