f0e84e0fd3332ddb2371b9b1aa8930a1969588f0
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-10-08 20:03:07 +0530

    Update pipeline_initialize.groovy

--- vars/pipeline_initialize.groovy
+++ vars/pipeline_initialize.groovy
@@ -10,7 +10,7 @@ def call() {
         extensions: [],
         submoduleCfg: [],
         userRemoteConfigs: [[
-            
+            credentialsId: 'git_cred', 
           url: "https://github.com/durgaprasad444/${name}.git"
              ]]])
    
@@ -21,6 +21,26 @@ def call() {
       case 'python':
         // Instantiate and execute a Python pipeline
             mine()
+   }
+   switch(p.pipelineType) {
+      case 'pythonexample':
+        // Instantiate and execute a Python pipeline
+            pythonexample()
+   }
+   switch(p.pipelineType) {
+      case 'mavenexample':
+        // Instantiate and execute a Python pipeline
+            mavenexample()
+   }
+   switch(p.pipelineType) {
+      case 'nodeexample':
+        // Instantiate and execute a Python pipeline
+            nodeexample()
+   }
+   switch(p.pipelineType) {
+      case 'dotnetexample':
+        // Instantiate and execute a Python pipeline
+            dotnetexample()
    }
    switch(p.pipelineType) {
       case 'maven':
