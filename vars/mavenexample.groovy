71cda5baff752d91b52b09d8928639b5ed69f628
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-10-08 20:47:43 +0530

    Update mavenexample.groovy

--- vars/mavenexample.groovy
+++ vars/mavenexample.groovy
@@ -1,11 +1,13 @@
 def call() {
   node {    
     def p = pipelineCfg()
+    stage('Build') {
                 sh """
                 cd /var/lib/jenkins/workspace/${JOB_NAME}/
                 echo 'Building..'
                 mvn clean install
                 docker build -t durgaprasad444/${p.appname}:v1 .
                 """
+    }
             }
         }
